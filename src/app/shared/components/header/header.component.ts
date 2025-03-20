c0c7ded7990636bf9eb66abb70f3705a43c36146
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2025-03-20 17:29:44 +0900

    Add header component

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [NgClass],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
