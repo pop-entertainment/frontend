29b7c253b40d6aa105d9fd06b92ce9d3b03092cb
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2025-03-20 17:37:41 +0900

    Move header to upper layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, MatButtonModule, HeaderComponent],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
