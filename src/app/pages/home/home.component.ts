03dce05473b8a0c8c7a6a41f56496c94e55ccaac
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2025-03-20 17:30:06 +0900

    Update home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
