eef1542085d05f1b3acb1e278b5870ce88b8625d
aalto95 <stanportableuse@gmail.com>
2025-04-10 18:22:08 +0900

    Fix unit test

--- src/app/pages/product/product.component.spec.ts
+++ src/app/pages/product/product.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductComponent } from './product.component';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../app.routes';
 
 describe('ProductComponent', () => {
   let component: ProductComponent;
@@ -8,9 +10,9 @@ describe('ProductComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductComponent]
-    })
-    .compileComponents();
+      imports: [ProductComponent],
+      providers: [provideRouter(routes)]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
