3704e965cea17980f5128d3a404e24c131e0a344
aalto95 <stanportableuse@gmail.com>
2025-04-17 13:03:57 +0900

    Rewrite getting products to observable and template async pipe

--- src/app/pages/home/goods-catalog/goods-catalog.component.ts
+++ src/app/pages/home/goods-catalog/goods-catalog.component.ts
@@ -1,22 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '../../../shared/components/product-card/product-card.component';
-import { ProductInfoModel } from '../../../shared/models';
 import { StoreService } from '../../../services';
 
 @Component({
   selector: 'app-goods-catalog',
-  imports: [ProductCardComponent],
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './goods-catalog.component.html',
   styleUrl: './goods-catalog.component.scss'
 })
-export class GoodsCatalogComponent implements OnInit {
+export class GoodsCatalogComponent {
   private storeService = inject(StoreService);
 
-  products: ProductInfoModel[] = [];
-
-  ngOnInit(): void {
-    this.storeService.getProducts().subscribe((products) => {
-      this.products = products;
-    });
-  }
+  products$ = this.storeService.getProducts();
 }
--- src/app/pages/home/popular-goods/popular-goods.component.ts
+++ src/app/pages/home/popular-goods/popular-goods.component.ts
@@ -1,22 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '../../../shared/components/product-card/product-card.component';
-import { ProductInfoModel } from '../../../shared/models';
 import { StoreService } from '../../../services';
 
 @Component({
   selector: 'app-popular-goods',
-  imports: [ProductCardComponent],
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './popular-goods.component.html',
   styleUrl: './popular-goods.component.scss'
 })
-export class PopularGoodsComponent implements OnInit {
+export class PopularGoodsComponent {
   private storeService = inject(StoreService);
 
-  products: ProductInfoModel[] = [];
-
-  ngOnInit(): void {
-    this.storeService.getProducts().subscribe((products) => {
-      this.products = products;
-    });
-  }
+  products$ = this.storeService.getProducts();
 }
